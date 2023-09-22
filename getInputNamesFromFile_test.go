1b6c107b2689d82403a84b0b5be519f01b1205ae
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-09-22 00:13:58 -0300

    update module: model fix TestGetNamesFromDirectoryExtensionAndPattern

--- getInputNamesFromFile_test.go
+++ getInputNamesFromFile_test.go
@@ -47,6 +47,7 @@ func TestGetNamesFromDirectoryExtensionAndPattern(t *testing.T) {
 		expected  []string
 	}{
 		"búsqueda en 2 archivos goo, Phone repetido": {"test", ".goo", model.INPUT_PATTERN, []string{"Phone", "RadioGenero", "TextArea"}},
+		"búsqueda en 2 archivos gol, ":               {"test/golfiles", ".gol", model.INPUT_PATTERN, []string{"Pk", "Text", "TextArea", "Date", "Hour"}},
 	}
 
 	for testName, data := range testData {
@@ -54,6 +55,9 @@ func TestGetNamesFromDirectoryExtensionAndPattern(t *testing.T) {
 
 			resp, err := gotools.GetNamesFromDirectoryExtensionAndPattern(data.file_path, data.extension, data.pattern)
 
+			if len(resp) != len(data.expected) {
+				log.Fatalf("\n-se esperaba\n%v\n-pero se obtuvo\n%v", data.expected, resp)
+			}
 			// Comparar elemento por elemento
 			for i, valor := range resp {
 				if valor != data.expected[i] {
